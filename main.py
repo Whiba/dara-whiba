df656d8a3d45a1d520169c6c42412424ad499e66
DariaMaltseva <dasha.dh@mail.ru>
2017-10-29 18:17:02 +0500

    Лабораторная работа №1. Загрузка текста из файла

--- main.py
+++ main.py
@@ -1,43 +1,58 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 
-import nltk
-import re
-import pymorphy2
-from nltk import Counter
+import nltk 
+import re 
+import sys 
+import pymorphy2 
+from nltk import Counter 
 
-morph = pymorphy2.MorphAnalyzer()
+morph = pymorphy2.MorphAnalyzer() 
 
-text = u'Вася и Катя пошли гулять в парк. Там они встретили своих друзей Костю, Дениса и Марину. ' \
-        u'Ребята играли в футбол. Вася и Катя поздоровались с друзьями и пошли на речку.';
-name_male = []
-name_female = []
+myfile = open("new1.txt", "rb"); 
+text = myfile.read(); 
+name_male = [] 
+name_female = [] 
+text_memoty = text; 
 
-# cut text by tokens and find names and divide them by sex
-for i in [',', '.', ':', ';', '!', '?']:
-    text = text.replace(i, '')
-mylist = text.split();
+# cut text by tokens and find names and divide them by sex 
+for i in [',', '.', ':', ';', '!', '?', '-', '—', '(', ')', '«', '»']: 
+    text_memoty = text_memoty.replace(i, ' ')
+mylist = text_memoty.split(); 
 
-for word in mylist:
+for word in mylist: 
     # выбираем с наибольшим score - оценка вероятности что данный набор правильный
     # так как сортировка по убыванию score, мы берем самый вероятный
-    p = morph.parse(word)[0];
+    myword = word.decode('utf-8')
+    p = morph.parse(myword)[0];
     if ('Name' in p.tag) == True:
-        print(word)
-        print(p.tag)
+        # print(word)
+        # print(p.tag)
         if ('masc' in p.tag) == True:
             name_male.append(p.normal_form)
         if ('femn' in p.tag) == True:
             name_female.append(p.normal_form)
 
-# search most popular male and female names
-cntr_m = Counter(name_male)
-popular_man = cntr_m.most_common(1)[0]
+# search most popular male and female names 
+cntr_m = Counter(name_male) 
+if cntr_m.__len__() != 0:
+    popular_man = cntr_m.most_common(1)[0]
+else: 
+    popular_man = ''
 
-cntr_f = Counter(name_female)
-popular_wom = cntr_f.most_common(1)[0]
+cntr_f = Counter(name_female) 
+if cntr_f.__len__() != 0: 
+    popular_wom = cntr_f.most_common(1)[0]
+else: 
+    popular_wom = ''
 
-# make a headline and print result
-print("-------------------------------------------------------------------------------")
-headline = popular_man[0] + u" и " + popular_wom[0]
-print(u"'" + headline + u"'")
+# make a headline and print result 
+print("-------------------------------------------------------------------------------") 
+if type(popular_man) != str and type(popular_wom) != str: 
+    headline = popular_man[0] + u" и " + popular_wom[0]
+else: 
+    if type(popular_man) == tuple and type(popular_wom) == str:
+        headline = u"Меня зовут " + popular_man[0]
+    if type(popular_wom) == tuple and type(popular_man) == str:
+        headline = u"Меня зовут " + popular_wom[0]
+print(u"'" + headline + u"'") 
 print(text)
